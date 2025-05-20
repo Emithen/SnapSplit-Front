b8b7b09e60e8eada9d59c062d8bada791334108c
Jisu <92006284+jsomnium@users.noreply.github.com>
2025-05-20 16:41:48 +0900

    feat: 시스템 전체 pretendard 폰트 적용

--- snapsplit/src/app/layout.tsx
+++ snapsplit/src/app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css';
 
 export const metadata: Metadata = {
   title: 'SnapScpit',
-  description: 'Trip & Photo Management',
+  description: 'Trip & Photo Management Service',
 };
 
 export default function RootLayout({
@@ -13,7 +13,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <head>
+        <link
+          rel="stylesheet"
+          as="style"
+          href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.8/dist/web/static/pretendard-dynamic-subset.css"
+          crossOrigin="anonymous"
+        />
+      </head>
+      <meta
+        name="viewport"
+        content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no,viewport-fit=cover"
+      />
+      <body className="h-[100dvh] min-w-[360px] max-w-[415px] lg:max-w-[360px] mx-auto bg-white scroll-smooth">
+        {children}
+      </body>
     </html>
   );
 }
