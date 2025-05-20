689343aca6f4c08afa8ebac7da46b0a7b1d3f2ea
Jisu <92006284+jsomnium@users.noreply.github.com>
2025-05-20 19:42:57 +0900

    feat: landing-header UI 구현

--- snapsplit/src/app/landing/page.tsx
+++ snapsplit/src/app/landing/page.tsx
@@ -1,7 +1,15 @@
 // 최초 랜딩 페이지
 
+import LandingHeader from '@/features/landing/components/LandingHeader';
+import CreateTrip from '@/features/landing/components/CreateTrip';
+
 const LandingPage = () => {
-  return <div>LandingPage</div>;
+  return (
+    <div>
+      <LandingHeader />
+      <CreateTrip />
+    </div>
+  );
 };
 
 export default LandingPage;
---
+++ snapsplit/src/features/landing/components/CreateTrip.tsx
@@ -0,0 +1,9 @@
+const CreateTrip = () => {
+  return (
+    <section className="p-4">
+      <span className="text-head-0 bg-grey flex-auto text-blue-900">스냅스플릿과 함께 재밌는 여행을 떠나볼까요?</span>
+    </section>
+  );
+};
+
+export default CreateTrip;
