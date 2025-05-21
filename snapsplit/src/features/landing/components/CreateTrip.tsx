5eaac8bd4098cc0a2430e4c59b483f16838d19b5
Jisu <92006284+jsomnium@users.noreply.github.com>
2025-05-21 09:45:43 +0900

    feat: tailwind.config 파일 인식

--- snapsplit/src/features/landing/components/CreateTrip.tsx
+++ snapsplit/src/features/landing/components/CreateTrip.tsx
@@ -1,7 +1,7 @@
 const CreateTrip = () => {
   return (
     <section className="p-4">
-      <span className="text-head-0 bg-grey flex-auto text-blue-900">스냅스플릿과 함께 재밌는 여행을 떠나볼까요?</span>
+      <span className="text-head-1 bg-grey flex-auto">스냅스플릿과 함께 재밌는 여행을 떠나볼까요?</span>
     </section>
   );
 };
