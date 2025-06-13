b8c5560a37f14f77ccaa71498dc19d7a848decae
Jisu <92006284+jsomnium@users.noreply.github.com>
2025-06-13 14:55:04 +0900

    feat: nextStep implementation

--- snapsplit/src/features/trip/createTrip/CreateTripPage.tsx
+++ snapsplit/src/features/trip/createTrip/CreateTripPage.tsx
@@ -1,16 +1,40 @@
+'use client';
+import { useState } from 'react';
+
 import StepProgressBar from '@trip/createTrip/_components/StepProgressBar';
 import CreateTripHeader from '@trip/createTrip/_components/CreateTripHeader';
 import CountrySearchSection from '@trip/createTrip/steps/Step1_CountrySearch';
+import SelectDateSection from './steps/Step2_SelectDate';
 import AddMemberSection from '@trip/createTrip/steps/Step3_AddMember';
 import InputTripName from './steps/Step4_InputTripName';
 
 // steps로 단계별 컴포넌트를 랜더링해주는 Multi Step Form 페이지
 export default function CreateTripPage() {
+  const [step, setStep] = useState(1);
+
+  /* 스탭 이동 테스트 */
+  const goNext = () => setStep((prev) => Math.min(prev + 1, 4));
+  const goPrev = () => setStep((prev) => Math.max(prev - 1, 1));
+
   return (
     <div>
-      <CreateTripHeader />
-      <StepProgressBar step={1} />
-      <InputTripName />
+      <div>
+        <CreateTripHeader />
+        <StepProgressBar step={step} />
+
+        {step === 1 && <CountrySearchSection />}
+        {step === 2 && <SelectDateSection />}
+        {step === 3 && <AddMemberSection />}
+        {step === 4 && <InputTripName />}
+
+        {/* 스탭 이동 테스트 */}
+        <button onClick={goPrev} disabled={step === 1}>
+          이전
+        </button>
+        <button onClick={goNext} disabled={step === 4}>
+          다음
+        </button>
+      </div>
     </div>
   );
 }
--- snapsplit/src/features/trip/createTrip/steps/Step2_SelectDate/index.tsx
+++ snapsplit/src/features/trip/createTrip/steps/Step2_SelectDate/index.tsx
@@ -1,5 +1,12 @@
 const SelectDateSection = () => {
-  return <div className="">SelectDateSection</div>;
+  return (
+    <div className="px-5">
+      <div className="pb-6">
+        <p className="text-head-1">여행 일정을 등록해주세요</p>
+        <p className="text-body-2 text-grey-850">캘린더에서 떠나는 날과 돌아오는 날을 선택하세요</p>
+      </div>
+    </div>
+  );
 };
 
 export default SelectDateSection;
