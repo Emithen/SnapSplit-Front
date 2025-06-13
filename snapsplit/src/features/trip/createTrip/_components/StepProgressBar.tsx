897376c3a79cecf51dfbc38bab79bc276abf5587
Jisu <92006284+jsomnium@users.noreply.github.com>
2025-06-13 17:26:35 +0900

    style: 스탭 전환 애니메이션 적용

--- snapsplit/src/features/trip/createTrip/_components/StepProgressBar.tsx
+++ snapsplit/src/features/trip/createTrip/_components/StepProgressBar.tsx
@@ -1,8 +1,17 @@
+'use client';
+import { motion } from 'framer-motion';
+
 const StepProgressBar = ({ step }: { step: number }) => {
   return (
     <div className="flex gap-1 pt-[18px] pb-6 px-5">
       {[1, 2, 3, 4].map((i) => (
-        <div key={i} className={`h-1 flex-1 rounded  ${i <= step ? 'bg-grey-850' : 'bg-grey-350'}`} />
+        <motion.div
+          key={i}
+          initial={{ backgroundColor: '#D1D1D1' }} // grey-350
+          animate={{ backgroundColor: i <= step ? '#1F1F1F' : '#D1D1D1' }} // grey-850 or grey-350
+          transition={{ duration: 0.3 }}
+          className="h-1 flex-1 rounded"
+        />
       ))}
     </div>
   );
