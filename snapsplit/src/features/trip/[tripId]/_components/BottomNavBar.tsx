f6ae2e396c557992f525aa22de77d41fe4b15111
Jisu <92006284+jsomnium@users.noreply.github.com>
2025-06-15 15:06:01 +0900

    style: 총 지출 UI 디테일 조정

--- snapsplit/src/features/trip/[tripId]/_components/BottomNavBar.tsx
+++ snapsplit/src/features/trip/[tripId]/_components/BottomNavBar.tsx
@@ -1,6 +1,6 @@
 const BottomNavBar = () => {
   return (
-    <div className="w-96 px-2.5 pt-3 pb-1 bg-white inline-flex justify-start items-center overflow-hidden">
+    <div className="w-full px-2.5 pt-3 pb-1 bg-white inline-flex justify-start items-center overflow-hidden">
       <div className="flex-1 inline-flex flex-col justify-start items-center">
         <div className="w-6 h-6 inline-flex justify-center items-center">
           <div className="w-3.5 h-4 outline outline-[1.50px] outline-offset-[-0.75px] outline-neutral-700" />
--- snapsplit/src/features/trip/[tripId]/_components/TotalExpenseBottomSheet.tsx
+++ snapsplit/src/features/trip/[tripId]/_components/TotalExpenseBottomSheet.tsx
@@ -9,13 +9,16 @@ const TotalExpenseBottomSheet = ({ tripTotalExpense = 0 }: TotalExpenseBottomShe
   // 지출 금 "원" 으로 표기할지 상의
 
   return (
-    <div className="self-stretch px-2.5 pt-0.5 pb-4 bg-neutral-500 shadow-[0px_-2px_10px_0px_rgba(108,108,108,0.12)] inline-flex flex-col justify-center items-center">
-      <div className="w-6 h-6 flex flex-col justify-center items-center overflow-hidden">
-        <Image alt="↑" src={topArrow} />
-      </div>
-      <div className="justify-start text-neutral-50 text-lg font-semibold font-['Pretendard'] leading-relaxed">
-        총 {tripTotalExpense.toLocaleString()}원 지출
-      </div>
+    <div className="relative flex flex-col items-center bg-neutral-500 pt-[22px] pb-[18px]">
+      {/* 위쪽 화살표 */}
+      <Image
+        alt="↑"
+        src={topArrow}
+        className="absolute top-[2px]" // 겹치게 위로 끌어올림
+      />
+
+      {/* 텍스트 */}
+      <div className="text-grey-50 text-title-1 z-10">총 {tripTotalExpense.toLocaleString()}원 지출</div>
     </div>
   );
 };
