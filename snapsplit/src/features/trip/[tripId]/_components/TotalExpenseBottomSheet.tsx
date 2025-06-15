7caa0997412085eee0a7b2aad1d7c2802051f77a
Jisu <92006284+jsomnium@users.noreply.github.com>
2025-06-15 14:59:01 +0900

    feat: 총 지출금 바텀 시트 UI 개발

--- snapsplit/src/features/trip/[tripId]/_components/TotalExpenseBottomSheet.tsx
+++ snapsplit/src/features/trip/[tripId]/_components/TotalExpenseBottomSheet.tsx
@@ -1,11 +1,20 @@
-const TotalExpenseBottomSheet = () => {
+import Image from 'next/image';
+import topArrow from '@public/svg/topArrow.svg';
+
+export type TotalExpenseBottomSheetProps = {
+  tripTotalExpense?: number;
+};
+
+const TotalExpenseBottomSheet = ({ tripTotalExpense = 0 }: TotalExpenseBottomSheetProps) => {
+  // 지출 금 "원" 으로 표기할지 상의
+
   return (
     <div className="self-stretch px-2.5 pt-0.5 pb-4 bg-neutral-500 shadow-[0px_-2px_10px_0px_rgba(108,108,108,0.12)] inline-flex flex-col justify-center items-center">
       <div className="w-6 h-6 flex flex-col justify-center items-center overflow-hidden">
-        <div className="w-3.5 h-1.5 origin-top-left -rotate-180 outline outline-[1.50px] outline-offset-[-0.75px] outline-neutral-400" />
+        <Image alt="↑" src={topArrow} />
       </div>
       <div className="justify-start text-neutral-50 text-lg font-semibold font-['Pretendard'] leading-relaxed">
-        총 124,500원 지출
+        총 {tripTotalExpense.toLocaleString()}원 지출
       </div>
     </div>
   );
