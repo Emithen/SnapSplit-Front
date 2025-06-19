7470619485497ddeef0664d1501d5bd43ea71423
Jisu <92006284+jsomnium@users.noreply.github.com>
2025-06-19 09:21:31 +0900

    refactor: 불필요한 div 태그 제거

--- snapsplit/src/features/trip/[tripId]/_components/TotalExpenseBottomSheet.tsx
+++ snapsplit/src/features/trip/[tripId]/_components/TotalExpenseBottomSheet.tsx
@@ -9,18 +9,16 @@ const TotalExpenseBottomSheet = ({ tripTotalExpense = 0 }: TotalExpenseBottomShe
   // 지출 금 "원" 으로 표기할지 상의
 
   return (
-    <div>
-      <div className="relative flex flex-col items-center bg-neutral-500 pt-[22px] pb-[18px]">
-        {/* 위쪽 화살표 */}
-        <Image
-          alt="↑"
-          src={topArrow}
-          className="absolute top-[2px]" // 겹치게 위로 끌어올림
-        />
+    <div className="relative flex flex-col items-center bg-neutral-500 pt-[22px] pb-[18px]">
+      {/* 위쪽 화살표 */}
+      <Image
+        alt="총 지출 모달 열기"
+        src={topArrow}
+        className="absolute top-[2px]" // 겹치게 위로 끌어올림
+      />
 
-        {/* 텍스트 */}
-        <div className="text-grey-50 text-title-1 z-10">총 {tripTotalExpense.toLocaleString()}원 지출</div>
-      </div>
+      {/* 텍스트 */}
+      <div className="text-grey-50 text-title-1 z-10">총 {tripTotalExpense.toLocaleString()}원 지출</div>
     </div>
   );
 };
