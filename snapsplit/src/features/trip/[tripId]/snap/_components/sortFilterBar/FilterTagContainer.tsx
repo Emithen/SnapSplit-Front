78a2d66b6196908a10dbfb07aefd49b27b0e73e7
Emithen <86219540+Emithen@users.noreply.github.com>
2025-06-21 00:40:45 +0900

    design: padding 적용 범위 조정

--- snapsplit/src/features/trip/[tripId]/snap/_components/sortFilterBar/FilterTagContainer.tsx
+++ snapsplit/src/features/trip/[tripId]/snap/_components/sortFilterBar/FilterTagContainer.tsx
@@ -10,9 +10,9 @@ function FilterTagContainer({ filters }: FilterTagContainerProps) {
     const isTagExist = filters.days.length > 0 || filters.people.length > 0 || filters.locations.length > 0;
 
     return (
-      <div className="flex gap-1.5 pb-3">
+      <>
         {isTagExist && (
-          <>
+          <div className="flex gap-1.5 pb-3">
             <button className="flex justify-center items-center bg-grey-450 w-7 h-7 rounded-full">
               <Image src="/svg/refresh.svg" alt="refresh" width={12} height={12} />
             </button>
@@ -25,9 +25,9 @@ function FilterTagContainer({ filters }: FilterTagContainerProps) {
             {filters.locations.length > 0 && (
               <FilterTag label={filters.locations.join(', ')} />
             )}
-          </>
+          </div>
         )}
-      </div>
+      </>
     );
 }
 
