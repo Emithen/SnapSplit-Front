cc4eb2f0f7f8f240ecefa02e317e28ba172dcbe4
Jisu <92006284+jsomnium@users.noreply.github.com>
2025-06-23 09:44:38 +0900

    feat: BottomNavBar 적용
    - 나머지 패딩 등 수정

--- snapsplit/src/features/trip/[tripId]/_components/DailyExpenseList.tsx
+++ snapsplit/src/features/trip/[tripId]/_components/DailyExpenseList.tsx
@@ -16,7 +16,7 @@ const DailyExpenseList = ({ expenses, tripStartDate, tripEndDate }: DailyExpense
   return (
     <div
       id="scroll-target-top"
-      className="flex-grow w-full space-y-8 px-5 text-grey-850 pb-[100px] overflow-y-auto scrollbar-hide"
+      className="flex-grow w-full space-y-8 px-5 text-grey-850 pb-[159px] overflow-y-auto scrollbar-hide"
     >
       {groupedExpenses.map((group) => (
         <div key={group.label} className="space-y-3" id={group.id ? `day-${group.id}` : undefined}>
