9967ade6d8c5e71b3a4641fd22da4b93d3586d47
Jisu <92006284+jsomnium@users.noreply.github.com>
2025-07-03 03:17:12 +0900

    feat: 모션 적용 & 카카오 로그인 버튼 추가

--- snapsplit/src/features/landing/_components/landingSection.tsx
+++ snapsplit/src/features/landing/_components/landingSection.tsx
@@ -1,23 +1,53 @@
+'use client';
+
+import { useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
+import { motion, AnimatePresence } from 'framer-motion';
 import landing_logo from '@public/svg/landing-logo.svg';
 
 const LandingSection = () => {
+  const [showKakao, setShowKakao] = useState(false);
+
   return (
     <main className="flex flex-col justify-between w-full h-screen p-6 bg-gradient-to-br from-[#B9F5E2] via-white to-[#DDF9F7]">
       {/* 상단 콘텐츠 */}
       <div className="flex flex-col pt-44 gap-5 items-center">
         <Image src={landing_logo} alt="SnapSplit" />
         <div className="flex flex-col items-center text-[16px] font-light text-primary justify-center">
-          <p className="flex items-center justify-center">Snap the memories,</p>
-          <p className="flex items-center justify-center">Split the costs</p>
+          <p>Snap the memories,</p>
+          <p>Split the costs</p>
         </div>
       </div>
 
       {/* 하단 고정 버튼 */}
-      <Link href="/home" className="w-full bg-primary flex justify-center p-4 rounded-2xl text-white">
-        시작하기
-      </Link>
+      <AnimatePresence mode="wait">
+        {!showKakao ? (
+          <motion.button
+            key="start"
+            initial={{ opacity: 0, x: 100 }}
+            animate={{ opacity: 1, x: 0 }}
+            exit={{ opacity: 0, x: -100 }}
+            transition={{ duration: 0.5, ease: 'easeInOut' }}
+            className="w-full bg-primary flex justify-center p-4 rounded-2xl text-white cursor-pointer"
+            onClick={() => setShowKakao(true)}
+          >
+            시작하기
+          </motion.button>
+        ) : (
+          <Link href="/home" key="kakao">
+            <motion.div
+              initial={{ opacity: 0, x: 100 }}
+              animate={{ opacity: 1, x: 0 }}
+              exit={{ opacity: 0, x: -100 }}
+              transition={{ duration: 0.5, ease: 'easeInOut' }}
+              className="w-full bg-yellow-300 flex justify-center p-3 rounded-2xl text-black cursor-pointer"
+            >
+              카카오 로그인
+            </motion.div>
+          </Link>
+        )}
+      </AnimatePresence>
     </main>
   );
 };
