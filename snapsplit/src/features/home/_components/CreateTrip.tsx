fe039588ccd36caa765cda047bd97309dcab2e40
Jisu <92006284+jsomnium@users.noreply.github.com>
2025-07-03 00:41:32 +0900

    feat: 좌우 스크롤 기능 추가

--- snapsplit/src/features/home/_components/CreateTrip.tsx
+++ snapsplit/src/features/home/_components/CreateTrip.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import CurrentTriplist from './CurrentTripList';
+import CurrentTripList from './CurrentTripList';
 
 // 로그인 여부를 가져옴
 // 로그인 했을 시,
@@ -12,7 +12,7 @@ const CreateTrip = () => {
         스냅스플릿과 함께
         <br /> 재밌는 여행을 떠나볼까요?
       </span>
-      <CurrentTriplist />
+      <CurrentTripList />
       <div className="flex flex-col w-full gap-4">
         <Link href="/trip/createTrip" className="flex bg-primary py-[14px] px-5 justify-center text-grey-50 rounded-xl">
           여행 등록하기
