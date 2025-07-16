367cc07262e34ed3e789a3c35d1e91f94cab4a0b
Emithen <86219540+Emithen@users.noreply.github.com>
2025-07-16 20:18:51 +0900

    design: step2 페이지 디자인 변경

--- snapsplit/src/features/trip/createTrip/steps/Step2_SelectDate/TripDatePicker.tsx
+++ snapsplit/src/features/trip/createTrip/steps/Step2_SelectDate/TripDatePicker.tsx
@@ -11,10 +11,10 @@ const TripDatePicker = () => {
       {/* 떠나는 날 & 돌아오는 날 */}
       <div className="flex gap-2.5 w-full">
         <div className="flex-1 rounded-[12px] border-1 border-grey-250">
-          <div className="h-12 px-4 flex items-center text-body-2 text-grey-450">{startDate ? format(startDate, 'yyyy년 M월 d일') : '떠나는 날'}</div>
+          <div className={`h-12 px-4 flex items-center text-body-2 ${startDate ? 'text-grey-1000' : 'text-grey-450'}`}>{startDate ? format(startDate, 'yyyy. M. d') : '떠나는 날'}</div>
         </div>
         <div className="flex-1 rounded-[12px] border-1 border-grey-250">
-          <div className="h-12 px-4 flex items-center text-body-2 text-grey-450">{endDate ? format(endDate, 'yyyy년 M월 d일') : '돌아오는 날'}</div>
+          <div className={`h-12 px-4 flex items-center text-body-2 ${endDate ? 'text-grey-1000' : 'text-grey-450'}`}>{endDate ? format(endDate, 'yyyy. M. d') : '돌아오는 날'}</div>
         </div>
       </div>
 
