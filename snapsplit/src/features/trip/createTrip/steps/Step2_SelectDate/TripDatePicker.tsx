2e740a7a7744b8bc98b3570ad7ac9f4de4258c13
Jisu <92006284+jsomnium@users.noreply.github.com>
2025-07-21 15:29:51 +0900

    refactor: 변수명 변경 및 페이지 내에서 상태 관리 하도록 변경

--- snapsplit/src/features/trip/createTrip/steps/Step2_SelectDate/TripDatePicker.tsx
+++ snapsplit/src/features/trip/createTrip/steps/Step2_SelectDate/TripDatePicker.tsx
@@ -4,21 +4,29 @@ import { format, isSameDay } from 'date-fns';
 type TripDatePickerProps = {
   startDate: Date | null;
   endDate: Date | null;
-  onStartDateChange: (date: Date | null) => void;
-  onEndDateChange: (date: Date | null) => void;
+  setStartDate: (date: Date | null) => void;
+  setEndDate: (date: Date | null) => void;
 };
 
-const TripDatePicker = ({ startDate, endDate, onStartDateChange, onEndDateChange }: TripDatePickerProps) => {
-
+const TripDatePicker = ({
+  startDate,
+  endDate,
+  setStartDate: setStartDate,
+  setEndDate: setEndDate,
+}: TripDatePickerProps) => {
   return (
     <div className="flex flex-col items-center gap-5 ">
       {/* 떠나는 날 & 돌아오는 날 */}
       <div className="flex gap-2.5 w-full">
         <div className="flex-1 rounded-[12px] border-1 border-grey-250">
-          <div className={`h-12 px-4 flex items-center text-body-3 ${startDate ? 'text-grey-1000' : 'text-grey-450'}`}>{startDate ? format(startDate, 'yyyy. M. d') : '떠나는 날'}</div>
+          <div className={`h-12 px-4 flex items-center text-body-3 ${startDate ? 'text-grey-1000' : 'text-grey-450'}`}>
+            {startDate ? format(startDate, 'yyyy. M. d') : '떠나는 날'}
+          </div>
         </div>
         <div className="flex-1 rounded-[12px] border-1 border-grey-250">
-          <div className={`h-12 px-4 flex items-center text-body-3 ${endDate ? 'text-grey-1000' : 'text-grey-450'}`}>{endDate ? format(endDate, 'yyyy. M. d') : '돌아오는 날'}</div>
+          <div className={`h-12 px-4 flex items-center text-body-3 ${endDate ? 'text-grey-1000' : 'text-grey-450'}`}>
+            {endDate ? format(endDate, 'yyyy. M. d') : '돌아오는 날'}
+          </div>
         </div>
       </div>
 
@@ -29,18 +37,18 @@ const TripDatePicker = ({ startDate, endDate, onStartDateChange, onEndDateChange
           selectedRange={{ start: startDate, end: endDate }}
           onSelectDate={(date) => {
             if (!startDate || (startDate && endDate)) {
-              onStartDateChange(date);
-              onEndDateChange(null);
+              setStartDate(date);
+              setEndDate(null);
             } else if (startDate && !endDate) {
-                if (date < startDate) {
-                    onEndDateChange(startDate);
-                    onStartDateChange(date);
-                } else if (isSameDay(date, startDate)) {
-                    onStartDateChange(null);
-                    onEndDateChange(null);
-                } else {
-                    onEndDateChange(date);
-                }
+              if (date < startDate) {
+                setEndDate(startDate);
+                setStartDate(date);
+              } else if (isSameDay(date, startDate)) {
+                setStartDate(null);
+                setEndDate(null);
+              } else {
+                setEndDate(date);
+              }
             }
           }}
         />
--- snapsplit/src/features/trip/createTrip/steps/Step2_SelectDate/index.tsx
+++ snapsplit/src/features/trip/createTrip/steps/Step2_SelectDate/index.tsx
@@ -3,12 +3,14 @@
 import BottomCTAButton from '@/shared/components/BottomCTAButton';
 import { SelectDateSectionProps } from './type';
 import TripDatePicker from './TripDatePicker';
-import { useState } from 'react';
 
-const SelectDateSection = ({ onClick: handleNextStep }: SelectDateSectionProps) => {
-  const [startDate, setStartDate] = useState<Date | null>(null);
-  const [endDate, setEndDate] = useState<Date | null>(null);
-
+const SelectDateSection = ({
+  onClick: handleNextStep,
+  startDate,
+  endDate,
+  setStartDate,
+  setEndDate,
+}: SelectDateSectionProps) => {
   return (
     <div className="flex flex-col px-5" style={{ height: 'calc(100vh - 95px - 16px)' }}>
       <div className="pb-6">
@@ -19,21 +21,19 @@ const SelectDateSection = ({ onClick: handleNextStep }: SelectDateSectionProps) 
         <TripDatePicker
           startDate={startDate}
           endDate={endDate}
-          onStartDateChange={(newStartDate: Date | null) => {
+          setStartDate={(newStartDate: Date | null) => {
             setStartDate(newStartDate);
           }}
-          onEndDateChange={(newEndDate: Date | null) => {
+          setEndDate={(newEndDate: Date | null) => {
             setEndDate(newEndDate);
           }}
         />
       </div>
-      {
-        startDate && (
-          <div className="py-5">
-            <BottomCTAButton label={endDate ? '다음으로' : '당일 일정으로 선택'} onClick={handleNextStep} />
-          </div>
-        )
-      }
+      {startDate && (
+        <div className="py-5">
+          <BottomCTAButton label={endDate ? '다음으로' : '당일 일정으로 선택'} onClick={handleNextStep} />
+        </div>
+      )}
     </div>
   );
 };
