ded587dc3afd74f91f7bccf497a8d2645ad425ca
Emithen <86219540+Emithen@users.noreply.github.com>
2025-07-23 16:09:13 +0900

    feat: 필터 태그 바에 스크롤 추가

--- snapsplit/src/features/trip/[tripId]/snap/_components/sortFilterBar/FilterTagContainer.tsx
+++ snapsplit/src/features/trip/[tripId]/snap/_components/sortFilterBar/FilterTagContainer.tsx
@@ -1,37 +1,30 @@
-import { FilterState } from "../../type";
-import FilterTag from "./FilterTag";
-import Image from "next/image";
+import { FilterState } from '../../type';
+import FilterTag from './FilterTag';
+import Image from 'next/image';
 
 type FilterTagContainerProps = {
-    filters: FilterState;
-}
+  filters: FilterState;
+};
 
 function FilterTagContainer({ filters }: FilterTagContainerProps) {
-    const isTagExist = filters.days.length > 0 || filters.people.length > 0 || filters.locations.length > 0;
+  const isTagExist = filters.days.length > 0 || filters.people.length > 0 || filters.locations.length > 0;
 
-
-    if (!isTagExist) {
-        return null;
-    } else {
-        return (
-          <div className="flex-1 flex items-center gap-1.5 min-h-8">
-            <button className="flex-shrink-0 flex justify-center items-center bg-primary w-7 h-7 rounded-full">
-              <Image src="/svg/refresh.svg" alt="refresh" width={12} height={12} />
-            </button>
-            <div className="flex gap-1.5 overflow-x-auto">
-            {filters.days.length > 0 && (
-              <FilterTag label={`Day ${filters.days.join(', ')}`} />
-            )}
-            {filters.people.length > 0 && (
-              <FilterTag label={filters.people.join(', ')} />
-            )}
-            {filters.locations.length > 0 && (
-              <FilterTag label={filters.locations.join(', ')} />
-            )}
-            </div>
-          </div>
-        )
-    }
+  if (!isTagExist) {
+    return null;
+  } else {
+    return (
+      <div className="flex-1 flex items-center gap-1.5 min-h-8">
+        <button className="flex-shrink-0 flex justify-center items-center bg-primary w-7 h-7 rounded-full">
+          <Image src="/svg/refresh.svg" alt="refresh" width={12} height={12} />
+        </button>
+        <div className="flex gap-1.5 overflow-x-auto scrollbar-hide">
+          {filters.days.length > 0 && <FilterTag label={`Day ${filters.days.join(', ')}`} />}
+          {filters.people.length > 0 && <FilterTag label={filters.people.join(', ')} />}
+          {filters.locations.length > 0 && <FilterTag label={filters.locations.join(', ')} />}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default FilterTagContainer;
