8a7a0d8081d52fecd3b5a00d23fdb865b7633fc4
Jisu <92006284+jsomnium@users.noreply.github.com>
2025-08-01 19:04:47 +0900

    feat: 개별 지출 상세 데이터 랜더링

--- snapsplit/src/features/trip/[tripId]/split/[settlementId]/[memberId]/_components/TotalAmountInfo.tsx
+++ snapsplit/src/features/trip/[tripId]/split/[settlementId]/[memberId]/_components/TotalAmountInfo.tsx
@@ -3,7 +3,7 @@ import { TotalAmountInfoProps } from '../type';
 export default function TotalAmountInfo({ name, totalAmount }: TotalAmountInfoProps) {
   if (name === '공동경비') {
     return (
-      <div className="px-5 py-3 text-head-1 pb-5">
+      <div className="px-5 py-3 text-head-1">
         이번 여행에서 공동경비로
         <br />
         <span className="text-primary">{totalAmount}원</span>을 썼어요!
@@ -12,7 +12,7 @@ export default function TotalAmountInfo({ name, totalAmount }: TotalAmountInfoPr
   }
 
   return (
-    <div className="px-5 py-3 text-head-1 pb-5">
+    <div className="px-5 py-3 text-head-1">
       {name}님은 이번 여행에서
       <br />
       <span className="text-primary">{totalAmount}원</span>을 썼어요!
