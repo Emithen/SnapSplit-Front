8e09beff327817baf332a269a1329dc92eef6c22
Jisu <92006284+jsomnium@users.noreply.github.com>
2025-08-01 19:58:57 +0900

    feat: 지출금 UI 추가

---
+++ snapsplit/src/features/trip/[tripId]/budget/[expenseId]/_components/ExpenseAmount.tsx
@@ -0,0 +1,8 @@
+export default function ExpenseAmount() {
+  return (
+    <div className="px-5 py-4 bg-grey-150 flex flex-col rounded-xl">
+      <p className="text-head-1">500€</p>
+      <p className="text-body-2 text-grey-550">= 683,994.53원</p>
+    </div>
+  );
+}
