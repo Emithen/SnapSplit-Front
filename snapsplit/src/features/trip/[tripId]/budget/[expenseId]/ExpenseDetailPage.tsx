9d9dadcc6a991598c03b93467cfb280f735586b4
Jisu <92006284+jsomnium@users.noreply.github.com>
2025-08-02 00:10:59 +0900

    feat: 지출금 상세 페이지 추가

--- snapsplit/src/features/trip/[tripId]/budget/[expenseId]/ExpenseDetailPage.tsx
+++ snapsplit/src/features/trip/[tripId]/budget/[expenseId]/ExpenseDetailPage.tsx
@@ -20,11 +20,11 @@ export default function ExpenseDetailPage({ tripId }: ExpenseDetailPageProps) {
   return (
     <div className="h-screen w-full flex flex-col">
       <ExpenseDetailHeader />
-      <div className="flex flex-col w-full overflow-y-auto scrollbar-hide p-5">
+      <div className="flex h-full flex-col w-full overflow-y-auto scrollbar-hide p-5">
         <ExpenseAmount />
 
         {/* Detail Fields */}
-        <div className="mx-5 bg-light_grey rounded-xl space-y-3">
+        <div className="pt-6 space-y-6">
           {[
             { label: '여행 일자', value: 'Day 1' },
             { label: '지출 형태', value: '카드' },
@@ -32,42 +32,38 @@ export default function ExpenseDetailPage({ tripId }: ExpenseDetailPageProps) {
             { label: '지출 내용 (선택)', value: '스카이스캐너' },
             { label: '카테고리 (선택)', value: '항공' },
           ].map((item) => (
-            <div key={item.label} className="flex justify-between items-center">
-              <span className="text-body-2 text-grey-850">{item.label}</span>
-              <span className="text-body-2">{item.value}</span>
+            <div key={item.label} className="flex justify-between items-center text-body-2">
+              <span className="text-grey-550">{item.label}</span>
+              <span>{item.value}</span>
             </div>
           ))}
-        </div>
-
-        {/* Payers and Settlement */}
-        <div className="mx-5 bg-white rounded-xl mt-3 space-y-3">
-          {/* Payers */}
-          <div className="flex justify-between items-center">
-            <span className="text-body-2 text-grey-850">결제자</span>
-            <span className="text-body-2 text-grey-850">결제금액</span>
-          </div>
-          <div className="flex justify-between items-center">
-            <span className="text-body-1">공동경비</span>
-            <span className="text-body-1">440€</span>
-          </div>
-          <div className="flex justify-between items-center">
-            <span className="text-body-1">홍길동</span>
-            <span className="text-body-1">60€</span>
-          </div>
-
-          {/* Divider */}
-          <div className="border-t border-grey-200 pt-3 flex justify-between items-center">
-            <span className="text-body-2 text-grey-850">정산포함</span>
-            <span className="text-body-2 text-grey-850">부담금액</span>
-          </div>
-
-          {/* Burdens */}
-          {['홍길동', '김철수', '이영희'].map((name, idx) => (
-            <div key={idx} className="flex justify-between items-center">
-              <span className="text-body-1">{name}</span>
-              <span className="text-body-1">20€</span>
+          {/* Payers and Settlement */}
+          <div className="space-y-3">
+            <div className="flex justify-between items-center text-body-2">
+              <span className="text-grey-550">결제자</span>
+              <span className=" text-grey-550">결제금액</span>
             </div>
-          ))}
+            <div className="flex justify-between items-center text-body-1">
+              <span>공동경비</span>
+              <span>440€</span>
+            </div>
+            <div className="flex justify-between items-center text-body-1">
+              <span>홍길동</span>
+              <span>60€</span>
+            </div>
+          </div>
+          <div className="space-y-3">
+            <div className="flex justify-between items-center text-body-2 text-grey-550">
+              <span>정산포함</span>
+              <span>부담금액</span>
+            </div>
+            {['홍길동', '김철수', '이영희'].map((name, idx) => (
+              <div key={idx} className="flex justify-between items-center text-body-1">
+                <span>{name}</span>
+                <span>20€</span>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
