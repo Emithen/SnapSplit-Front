0482660802a479b59af31b158e56a56947875086
Emithen <86219540+Emithen@users.noreply.github.com>
2025-08-01 19:29:02 +0900

    feat: kakao 인가 코드 받는 흐름에서 예외 처리

--- snapsplit/src/app/(no-gnb)/auth/page.tsx
+++ snapsplit/src/app/(no-gnb)/auth/page.tsx
@@ -1,11 +1,34 @@
 'use client';
 
+import { useRouter } from 'next/navigation';
+
 export default function Auth() {
   const REST_API_KEY = process.env.NEXT_PUBLIC_REST_API_KEY;
   const REDIRECT_URI = process.env.NEXT_PUBLIC_REDIRECT_URI;
 
+  const router = useRouter();
+
   const kakaoLogin = () => {
-    window.location.href = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
-  }
+    // 환경 변수가 제대로 설정되어 있는지 확인
+    if (!REST_API_KEY || !REDIRECT_URI) {
+      console.error("환경 변수에 문제가 있습니다.", "REST_API_KEY : ", REST_API_KEY, "REDIRECT_URI : ", REDIRECT_URI);
+      alert("로그인에 실패했습니다.");
+      router.push('/');
+      return;
+    }
+
+    // 카카오 로그인 URL 생성
+    const kakaoLoginUrl = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+
+    // 카카오 로그인 시도
+    try {
+      window.location.href = kakaoLoginUrl;
+    } catch (error) {
+      console.error("Kakao 로그인 리다이렉트 실패 : ", error);
+      alert("로그인에 실패했습니다.");
+      router.push('/');
+    }
+  };
+
   return <button onClick={kakaoLogin} className="bg-amber-300 w-full py-5 px-4 cursor-pointer">로그인 버튼</button>;
 }
